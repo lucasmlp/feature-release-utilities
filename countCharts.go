a714a8182f8e22dfb34040c035111df7e6e72352
Lucas Lopes <lucas.lopes@suse.com>
2023-10-20 11:18:03 -0300

    Printing assets that have been port forwarded

--- countCharts.go
+++ countCharts.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"sort"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 )
@@ -35,6 +38,18 @@ func fileExists(path string) bool {
 	return !info.IsDir()
 }
 
+func checkLastCommitMessage(filePath string) (string, error) {
+	cmd := exec.Command("git", "log", "-n", "1", "--pretty=format:%s", filePath)
+	cmd.Dir = filepath.Dir(filePath)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+	return out.String(), nil
+}
+
 func main() {
 	releaseFilePath := "/Users/machado/development/suse/charts/release.yaml"
 	assetsDir := "/Users/machado/development/suse/charts/assets"
@@ -56,7 +71,14 @@ func main() {
 			filename := fmt.Sprintf("%s-%s.tgz", key, version)
 			filePath := filepath.Join(assetsDir, key, filename)
 			if fileExists(filePath) {
-				fmt.Printf("Found: %s\n", filename)
+				commitMsg, err := checkLastCommitMessage(filePath)
+				if err != nil {
+					fmt.Printf("Error checking commit for %s: %v\n", filename, err)
+					continue
+				}
+				if strings.Contains(commitMsg, "forward") || strings.Contains(commitMsg, "port") {
+					fmt.Printf("Chart: %s Version: %s has commit message containing 'forward' or 'port'. Message: %s\n", key, version, commitMsg)
+				}
 			} else {
 				fmt.Printf("Not found: %s\n", filename)
 			}
