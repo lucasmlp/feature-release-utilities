e322bee41369cd4a44a0090b236085cf5fe1c9c2
Lucas Lopes <lucas.lopes@suse.com>
2023-10-20 14:19:00 -0300

    Generating updated.yaml with updates from qa-signoff.yaml

--- main.go
+++ main.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"regexp"
-	"sort"
 	"strings"
 
 	"gopkg.in/yaml.v2"
@@ -31,111 +24,39 @@ func readYaml(filePath string) (ReleaseData, error) {
 	return data, nil
 }
 
-func writeYaml(filePath string, data ReleaseData) error {
-	content, err := yaml.Marshal(data)
-	if err != nil {
-		return err
-	}
-
-	return ioutil.WriteFile(filePath, content, 0644)
-}
-
-func writePrefixedYaml(filePath string, data ReleaseData) error {
-	content, err := yaml.Marshal(data)
-	if err != nil {
-		return err
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i := range lines {
-		if lines[i] != "" {
-			lines[i] = ":todo_added:\t" + lines[i]
+func generatePrefixedYaml(data ReleaseData, qaData ReleaseData) string {
+	var lines []string
+	for chart, versions := range data {
+		prefix := ":todo_added:\t"
+		if _, exists := qaData[chart]; exists {
+			prefix = ":todo_done:\t"
+		}
+		lines = append(lines, prefix+chart+":")
+		for _, version := range versions {
+			lines = append(lines, prefix+"- "+version)
 		}
 	}
-	prefixedContent := strings.Join(lines, "\n")
-
-	return ioutil.WriteFile(filePath, []byte(prefixedContent), 0644)
-}
-
-func fileExists(path string) bool {
-	info, err := os.Stat(path)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return !info.IsDir()
-}
-
-func checkLastCommitMessage(filePath string) (string, error) {
-	cmd := exec.Command("git", "log", "-n", "1", "--pretty=format:%s", filePath)
-	cmd.Dir = filepath.Dir(filePath)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		return "", err
-	}
-	return out.String(), nil
-}
-
-func hasCommitWords(commitMsg string) bool {
-	words := []string{"\\bforward-port\\b", "\\bport\\b", "\\bforward\\b", "\\bport-forward\\b"}
-	for _, word := range words {
-		if regexp.MustCompile(word).MatchString(commitMsg) {
-			return true
-		}
-	}
-	return false
+	return strings.Join(lines, "\n")
 }
 
 func main() {
 	releaseFilePath := "/Users/machado/development/suse/charts/release.yaml"
-	assetsDir := "/Users/machado/development/suse/charts/assets"
-	startFilePath := "start.yaml"
-	filteredFilePath := "filtered.yaml"
-	forwardPortFilePath := "forward-port.yaml"
+	qaSignoffFilePath := "qa-signoff.yaml"
+	updatedFilePath := "updated.yaml"
 
 	data, err := readYaml(releaseFilePath)
 	if err != nil {
-		log.Fatalf("Error reading YAML file: %v", err)
+		log.Fatalf("Error reading release.yaml: %v", err)
 	}
 
-	keys := make([]string, 0, len(data))
-	for key := range data {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys)
-
-	forwardPortData := make(ReleaseData)
-
-	for _, key := range keys {
-		for _, version := range data[key] {
-			filename := fmt.Sprintf("%s-%s.tgz", key, version)
-			filePath := filepath.Join(assetsDir, key, filename)
-			if fileExists(filePath) {
-				commitMsg, err := checkLastCommitMessage(filePath)
-				if err != nil {
-					continue
-				}
-				if hasCommitWords(commitMsg) {
-					forwardPortData[key] = append(forwardPortData[key], version)
-					delete(data, key)
-				}
-			}
-		}
-	}
-
-	err = writeYaml(forwardPortFilePath, forwardPortData)
+	qaData, err := readYaml(qaSignoffFilePath)
 	if err != nil {
-		log.Fatalf("Error writing to forward-port.yaml: %v", err)
+		log.Fatalf("Error reading qa-signoff.yaml: %v", err)
 	}
 
-	err = writePrefixedYaml(startFilePath, data)
+	updatedContent := generatePrefixedYaml(data, qaData)
+	err = ioutil.WriteFile(updatedFilePath, []byte(updatedContent), 0644)
 	if err != nil {
-		log.Fatalf("Error writing to start.yaml: %v", err)
-	}
-
-	err = writeYaml(filteredFilePath, data)
-	if err != nil {
-		log.Fatalf("Error writing to filtered.yaml: %v", err)
+		log.Fatalf("Error writing to updated.yaml: %v", err)
 	}
 }
