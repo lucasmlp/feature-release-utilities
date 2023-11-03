a9acd72459dd76979737dc07e51efb60aba18566
Lucas Lopes <lucas.lopes@suse.com>
2023-11-03 08:54:24 -0300

    Adding copy file functionality to utils package

---
+++ pkg/utils/copyFile.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"io/ioutil"
+)
+
+// CopyFile copies a file from src to dst.
+func CopyFile(src, dst string) error {
+	input, err := ioutil.ReadFile(src)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(dst, input, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
