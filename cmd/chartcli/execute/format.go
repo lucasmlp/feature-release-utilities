9277415afd6107e1ef222d9b878750e677fda902
Lucas Lopes <lucas.lopes@suse.com>
2023-11-03 09:23:15 -0300

    Modifying format.go to format only manually inputted file

--- cmd/chartcli/execute/format.go
+++ cmd/chartcli/execute/format.go
@@ -13,7 +13,6 @@ import (
 )
 
 func Format(cmd *cobra.Command, args []string) {
-	files := []string{"toBeReleased.yaml", "released.yaml"}
 	bkpDir := "bkp"
 	timeSuffix := time.Now().Format("20060102-150405")
 
@@ -29,39 +28,37 @@ func Format(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	for _, file := range files {
-		// Back up the file
-		bkpFile, err := backupFile(file, bkpDir, timeSuffix)
-		if err != nil {
-			fmt.Printf("Failed to backup file %s: %v\n", file, err)
-			continue
-		}
-
-		// Load the YAML file to be formatted
-		data, err := utils.ReadYaml(file)
-		if err != nil {
-			fmt.Printf("Failed to read %s: %v\n", file, err)
-			continue
-		}
-
-		// Consolidate versions and count duplicates
-		consolidatedData, duplicates := consolidateVersions(data)
-
-		// Print the number of duplicates
-		fmt.Printf("File %s had %d duplicated chart/versions\n", file, duplicates)
-
-		// Sort the data according to the release order
-		sortReleaseData(&consolidatedData, &releaseOrder)
-
-		// Write the sorted data back to the file
-		err = utils.WriteYaml(file, consolidatedData)
-		if err != nil {
-			fmt.Printf("Failed to write sorted data to %s: %v\n", file, err)
-			continue
-		}
-
-		fmt.Printf("Formatted and backed up %s to %s\n", file, bkpFile)
+	// Back up the file
+	bkpFile, err := backupFile(utils.ReleasedFilePath, bkpDir, timeSuffix)
+	if err != nil {
+		fmt.Printf("Failed to backup file %s: %v\n", utils.ReleasedFilePath, err)
+		return
 	}
+
+	// Load the YAML file to be formatted
+	data, err := utils.ReadYaml(utils.ReleasedFilePath)
+	if err != nil {
+		fmt.Printf("Failed to read %s: %v\n", utils.ReleasedFilePath, err)
+		return
+	}
+
+	// Consolidate versions and count duplicates
+	consolidatedData, duplicates := consolidateVersions(data)
+
+	// Print the number of duplicates
+	fmt.Printf("File %s had %d duplicated chart/versions\n", utils.ReleasedFilePath, duplicates)
+
+	// Sort the data according to the release order
+	sortReleaseData(&consolidatedData, &releaseOrder)
+
+	// Write the sorted data back to the file
+	err = utils.WriteYaml(utils.ReleasedFilePath, consolidatedData)
+	if err != nil {
+		fmt.Printf("Failed to write sorted data to %s: %v\n", utils.ReleasedFilePath, err)
+		return
+	}
+
+	fmt.Printf("Formatted and backed up %s to %s\n", utils.ReleasedFilePath, bkpFile)
 }
 
 func backupFile(file, bkpDir, timeSuffix string) (string, error) {
